97816cb014da2e7d59a9a58fa02e3f0f129f1683
Jeff Tocco <jnt369@gmail.com>
2023-10-25 12:29:15 -0400

    dynamically set image loaded to corresponding number rolled

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ const score0Element = document.getElementById("score--0");
 const score1Element = document.getElementById("score--1");
 const diceElement = document.querySelector(".dice");
 
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const btnDouble = document.querySelector(".btn--double");
+
 // Preconditions
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add("hidden");
+
+// Dice Roll
+btnRoll.addEventListener("click", function () {
+  // 1. Random dice roll generation
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display result
+  diceElement.classList.remove("hidden");
+  diceElement.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, next player's turn
+});
