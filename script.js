66b35e9d98d68e5a9ae0a766348c605642f0b3b2
Jeff Tocco <jnt369@gmail.com>
2023-10-25 12:21:35 -0400

    Selecting the dice element

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 "use strict";
 
+// Selecting elements
 const score0Element = document.getElementById("score--0");
 const score1Element = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+
+// Preconditions
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add("hidden");
