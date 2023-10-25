4e171b431dd26c5f02900a1db518aa3e4b5cea8a
Jeff Tocco <jnt369@gmail.com>
2023-10-25 12:34:09 -0400

    Displaying currentscore (on player 1 for now)

--- script.js
+++ script.js
@@ -10,10 +10,15 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 const btnDouble = document.querySelector(".btn--double");
 
+const current0Element = document.getElementById("current--0");
+const current1Element = document.getElementById("current--1");
+
 // Preconditions
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add("hidden");
+
+let currentScore = 0;
 
 // Dice Roll
 btnRoll.addEventListener("click", function () {
@@ -25,4 +30,10 @@ btnRoll.addEventListener("click", function () {
   diceElement.src = `dice-${dice}.png`;
 
   // 3. Check for rolled 1: if true, next player's turn
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0Element.textContent = currentScore;
+  } else {
+  }
 });
