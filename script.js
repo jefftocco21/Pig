a5ad4e8ae6b6f294e5cd462be3aac22c04f00482
Jeff Tocco <jnt369@gmail.com>
2024-01-16 12:49:33 -0500

    Need to make sure modal is open first

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
